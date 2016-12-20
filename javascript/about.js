0aaff690bc0d2106b8c08f73d4f2f97618c3b8fa
Vita Shubin <vshubin@usc.edu>
2016-12-20 02:34:32 -0800

    =script code cleanup

--- javascript/about.js
+++ javascript/about.js
@@ -68,18 +68,3 @@ $(document).ready(function () {
     });
 });
 
-(function($) {
-		$(function() {
-
-$('.dropdown-button').dropdown({
-      inDuration: 300,
-      outDuration: 225,
-      hover: true, // Activate on hover
-      belowOrigin: true, // Displays dropdown below the button
-      constrainwidth: false, 
-    }
-  );
-
-		}); // End Document Ready
-})(jQuery); // End of jQuery name space
-
